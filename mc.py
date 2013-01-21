0c73cc161f7558c7f0d67831e7702536267f3a15
Steven Smethurst <git@funvill.com>
2013-01-20 18:31:31 -0800

    Python version from Super Happy Hacker House 25
    Python version from Super Happy Hacker House 25 (19th Jan, 2013)

---
+++ mc.py
@@ -0,0 +1,23 @@
+import time; 
+
+print ("Start your Talk, You have 2 mins (120 secs)"); 
+time.sleep( 30 ) ; 
+print ("1 1/2 mins left or 90 seconds"); 
+time.sleep( 30 ) ; 
+print ("1 min left or 60 seconds");
+time.sleep( 30 ) ; 
+print ("30 secs left !"); 
+time.sleep( 20 ) ; 
+
+print ("10 secs left !"); 
+time.sleep( 5 ) ; 
+print ("4 secs left !"); 
+time.sleep( 1 ) ; 
+print ("3 secs left !"); 
+time.sleep( 3 ) ; 
+print ("2 secs left !"); 
+time.sleep( 2 ) ; 
+print ("1 secs left !"); 
+time.sleep( 1 ) ; 
+print ("Thank you ! "); 
+
